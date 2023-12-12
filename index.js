2d7f77674d2eb8ae25d4741dd94baba8514524c6
adrianosuprine <adrianosuprine@gmail.com>
2023-12-12 19:00:16 +0300

    Review strings

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const shortGreeting=`Welcome, ${currentUser[0]} !`;
+// const excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser}!`;
+// console.log(excitedWelcomeMessage);
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
